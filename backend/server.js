2e40c51184c55f512192ba89ab7ce3e0fcebb7da
Soham2103 <160402380+Soham2103@users.noreply.github.com>
2026-07-23 17:50:47 +0530

    Add app.listen

--- backend/server.js
+++ backend/server.js
@@ -50,8 +50,10 @@ const connectDB = async () => {
 };
 
 connectDB();
+
+
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
